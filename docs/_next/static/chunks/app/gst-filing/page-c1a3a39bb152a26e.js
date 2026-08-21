f9de946846fffaedce38cd6fce25e1b5e33bc1e2
keshavagarwalbiz-spec <keshavagarwalbiz@gmail.com>
2026-08-21 13:33:25 +0530

    feat: build dedicated pages for all footer tabs correctly

---
+++ docs/_next/static/chunks/app/gst-filing/page-c1a3a39bb152a26e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[730,995,301,184,290],{9499:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=9499)}),_N_E=n.O()}]);
